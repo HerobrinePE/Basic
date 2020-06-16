f383289211ac61bfd5a4f0f5e60d5ff5d35f511d
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-06-15 20:24:02 -0400

    Create Avatar.js

---
+++ commands/Info/Avatar.js
@@ -0,0 +1,14 @@
+const { RichEmbed } = require("discord.js");
+let embed = new RichEmbed();
+
+module.exports = {
+  name: "avatar",
+  aliases: ["a"],
+  category: "info",
+  description: "returns an avatar picture",
+  run: (client, message, args) => {
+  let usr = message.mentions.users.first()
+  if(!usr) return message.channel.send(embed.setImage(message.author.avatarURL))
+  message.channel.send(embed.setImage(usr.avatarURL));
+  }
+}
