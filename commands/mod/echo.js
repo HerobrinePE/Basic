a59266f96a9beab6e0083d18f43de7b6c66cee02
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-06-15 20:27:36 -0400

    Create echo.js

---
+++ commands/mod/echo.js
@@ -0,0 +1,17 @@
+const { RichEmbed } = require("discord.js");
+
+module.exports = {
+	name: "echo",
+  aliases: ["announce"],
+	category: "moderation",
+	description: "Message another chat using the bot",
+	run: (client, message, args)=>{
+    message.delete(1000)
+       if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You cant do that")
+        const text = message.content.split(" ").slice(2);
+            if(!text) message.channel.send("What Should i say?")
+        let mUser = message.mentions.channels.first()
+        if(!mUser) return message.reply("please Mention channel")
+        mUser.send(text.join(" "))
+	}
+}
