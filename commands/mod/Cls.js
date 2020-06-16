d8722230a73f1ef32b247fe4a31d6a5ca8a65d9f
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-06-15 20:27:06 -0400

    Create Cls.js

---
+++ commands/mod/Cls.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "clear",
+    aliases: ["cls", "purge"],
+    category: "moderation",
+    description: "Bulk deletes",
+    usage: "${prefix}clear {amount}",
+    run: (cli, message, args) => {
+  if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("permission denied")
+  if(!args[0]) return message.channel.send("how much shall i delete?");
+  message.channel.bulkDelete(args[0]).then(() => {
+    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+  });
+}
+}
