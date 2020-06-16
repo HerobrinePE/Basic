69363afc973095698ea52a02c1eec2c037517852
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-06-15 20:24:26 -0400

    Create Emoji.js

---
+++ commands/Info/Emoji.js
@@ -0,0 +1,16 @@
+const { RichEmbed } = require("discord.js");
+const rembed = new RichEmbed()
+
+module.exports = {
+	name: "emoji",
+  aliases: ["e"],
+	category: "info",
+	description: "returns user img as emoji",
+	run: (client, message, args)=>{
+    message.delete(2000)
+	 let mtn = message.mentions.users.first()
+     if(!mtn) return message.reply("please mention user")
+        rembed.setThumbnail(mtn.avatarURL)
+         message.channel.send(rembed)
+	}
+}
