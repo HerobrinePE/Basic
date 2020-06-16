587451e6132847c831f1c014a14babf67cf59f52
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-06-15 20:22:29 -0400

    Create Dm.js

---
+++ commands/dm/Dm.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: "dm",
+  aliases: ["a"],
+  category: "info",
+  description: "dm",
+  run: (client, message, args) => {
+    
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You cant access this")
+    
+    
+  let usr = message.mentions.users.first()
+  if(!usr) return message.reply("please mention user")
+  let text = message.content.split(" ").slice(2)
+  if(!text) return message.reply("Cant do that")
+    usr.send(text.join(" "))
+  }
+}
