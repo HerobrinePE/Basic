bc6399956a572fbfed3d93512357a8d0b19f0e2f
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-06-15 20:25:59 -0400

    Create Rand.js

---
+++ commands/Info/Rand.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "rand",
+    category: "info",
+    description: "returns a random interger of your number",
+    run: async (client, message, args) => {
+let text = args[0]
+if(!text) return message.reply("choose a number")
+if(isNaN(text)) return message.channel.send(`${text} `+"is not a number (letters are not accepted)")
+if(!isNaN(text))
+var math = Math.floor(Math.random() * text)
+message.channel.send(`your random value ${math}`)
+
+    }
+}
