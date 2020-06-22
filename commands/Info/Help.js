cf50972c880081288cfb2574800c88c67d7bb260
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-06-21 23:45:10 -0400

    Update Help.js

--- commands/Info/Help.js
+++ commands/Info/Help.js
@@ -29,7 +29,7 @@ function getAll(client, message) {
 
     const info = client.categories
         .map(cat => stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n${commands(cat)}`)
-        .reduce((string, category) => string + "\n" + category);
+        .reduce((string, category) => string + " " + category);
 
     return message.channel.send(embed.setDescription(info));
 }
