73e3329ac224484258c6525925d6ede4d8d86329
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-06-27 00:21:58 -0400

    Update kn.js

--- commands/Info/kn.js
+++ commands/Info/kn.js
@@ -1,9 +1,9 @@
 module.exports = {
-    name: "endloop",
+    name: "kill",
     category: "Info",
     run: async (client, msg, args) => {
-    if(!msg.member.hasPermission == "ADMINISTRATOR") return msg.reply("you cant kill this interval")
-    msg.channel.send("Turning off please wait")
+    if(message.author.id == "475435277444186114") return message.reply("Only owner can use this")
+        msg.channel.send("Turning off please wait")
             msg.reply("Loop ended").then(()=>{
 client.destroy()
 process.exit()
