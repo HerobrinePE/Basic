1ee0ea690661f1159fb7bcbd2907a5d572f89fd4
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-07-13 02:44:45 -0400

    Create rrp.js

---
+++ commands/Roles/rrp.js
@@ -0,0 +1,22 @@
+const {RichEmbed} = require("discord.js")
+module.exports = {
+  name: "rrp",
+  category:"Roles",
+  run: async (client, message, args) => {
+    let role = args[0];
+    if (!role) return message.reply("no role specified");
+    if (!message.member.hasPermission("ADMINISTRATOR"))
+      return message.reply("you gotta has that perms bro");
+    let rol = message.guild.roles.find("name", role);
+    if (!rol) return message.channel.send("Error Role Not found");
+let mes = args[1]
+var t = mes.toUpperCase()
+    rol.removePermissions(t);
+    const embed = new RichEmbed()
+    .setTitle("SetPermission")
+    .setDescription(`${message.author} has removed the ${rol} permission ${t}`)
+    .setColor("RANDOM")
+    .setFooter(`${t}`)
+    message.channel.send(embed)
+  }
+};
