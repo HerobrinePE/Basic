a418cd5ded93303041ebeb0275f9749380af0957
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-07-21 18:17:57 -0400

    Update Stat.js

--- commands/Info/Stat.js
+++ commands/Info/Stat.js
@@ -8,7 +8,7 @@ module.exports = {
     run: (client, message, args) => {
     const embed = new RichEmbed()
     .setThumbnail("https://cdn.glitch.com/34219f89-5da9-4107-b722-a71edf79699b%2Ftenor.gif?v=1590626769885")
-    .setTitle("<a:NitroBoost728:709614699041259570>   Stats   <a:NitroBoost728:709614699041259570>")
+    .setTitle("<a:Fire:514567641449627648>   Stats   <a:Fire:514567641449627648>")
     .setDescription("-------------")
     .setColor("RANDOM")
     .addField("Command called by", message.author.username)
@@ -17,7 +17,7 @@ module.exports = {
     .addField("This Server Membercount", message.guild.memberCount)
     .addField('This Server Channel Count', message.guild.channels.size)
     .addField('This Server Role Count', message.guild.roles.size+"\n || ||")
-    .addField("\n \n<a:NitroBoost728:709614699041259570> Support by Subbing <a:NitroBoost728:709614699041259570>", "[Subscribe to Rainbowed](https://www.youtube.com/channel/UCGEzUyVKGktUkSkEasBwDeQ)")
+    .addField("\n \n<a:Fire:514567641449627648> Support by Subbing <a:Fire:514567641449627648>", "[Subscribe to Rainbowed](https://www.youtube.com/channel/UCGEzUyVKGktUkSkEasBwDeQ)")
     .addField("<a:Fire:514567641449627648> Support by Following <a:Fire:514567641449627648>", "[Follow Rainbowed on Twitter](https://twitter.com/RainbowStevePE)")
  message.channel.send(embed)
             
