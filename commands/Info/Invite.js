6514e643eda7d7ac05a7c6ec58e8711d7a43fed9
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-07-21 18:01:06 -0400

    Update Invite.js

--- commands/Info/Invite.js
+++ commands/Info/Invite.js
@@ -1,20 +1,23 @@
+const cdt = 86400;
 const cd = new Set();
 module.exports = {
   name: "tinvite",
   category: "Info",
   run: async (client, message, args) => {
-    if(cd.has(message.author.id)) {
-    message.reply("This command is on cooldown please wait 1 day to get another invite link")
-    }else{
-    send()
+    if (cd.has(message.author.id)) {
+      message.reply(
+        "This command is on cooldown please wait 1 day to get another invite link"
+      );
+    } else {
+      send();
     }
-    if(!message.member.hasPermission("ADMINISTRATOR")){ 
-      cd.add(message.author.id)
+    if (!message.member.hasPermission("ADMINISTRATOR")) {
+      cd.add(message.author.id);
     }
-    setTimeout(function(){
-    cd.delete(message.author.id)
-}, 86400)
-    async function send(){
+    var t = setTimeout(function() {
+      cd.delete(message.author.id);
+    }, cdt * 1000);
+    async function send() {
       try {
         let invite = await message.channel
           .createInvite({
