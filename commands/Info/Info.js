5a1650b33da44d05380ddeedd3560e07184c451f
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-08-08 06:29:49 -0400

    Update Info.js

--- commands/Info/Info.js
+++ commands/Info/Info.js
@@ -9,7 +9,7 @@ module.exports = {
     .addField("\n Main Developer", "[HerobrinePE](https://www.youtube.com/c/HerobrinePE)")
     .addField("\n Secondary Developer", "[Herobrina Briggs](https://www.youtube.com/channel/UCAXPslFRk1MOYPq9U1YJZPg)")
     .addField("In Collaboration with", "[Cloud Developers](https://discord.gg/w5fDZ3W)")
-    .addField("And", " [Orange Echo Stable](https://discordapp.com/oauth2/authorize?client_id=714001186897788934&scope=bot&permissions=2146958847)")
+    .addField("And", " [Orange Group](https://discord.gg/q39C4Sj) the creator of [Orange Echo Stable](https://discordapp.com/oauth2/authorize?client_id=714001186897788934&scope=bot&permissions=2146958847)")
     .addField("Devs Discord name", "Orange Group#5362, HerobrinePE#2365, Rainbowed#2261, Herobrina#3229, NC_Legend#2147, Skies#8245, Reverse#0001, chrhol#5015")
     .setFooter(`Command called by ${message.author.username}`)
     message.channel.send(b)
