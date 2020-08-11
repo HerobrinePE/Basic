4fa9d726b4b94596906130d5f1c5173bb671048f
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-08-11 15:10:21 -0400

    Update dm.js

--- commands/dm/dm.js
+++ commands/dm/dm.js
@@ -4,14 +4,27 @@ module.exports = {
   category: "dm",
   description: "dm",
   run: (client, message, args) => {
-    
-        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You cant access this")
-    
-    
-  let usr = message.mentions.users.first()
-  if(!usr) return message.reply("please mention user")
-  let text = message.content.split(" ").slice(2)
-  if(!text) return message.reply("Cant do that")
-    usr.send(text.join(" "))
+    let owner = "475435277444186114";
+    let ids = message.author;
+    if (!ids.id == owner) {
+      return error();
+    } else return send();
+    function error() {
+      const { RichEmbed } = require("discord.js");
+      const b = new RichEmbed()
+        .setTitle("Owners only command")
+        .setDescription("only <@" + owner + "> has access to thos command")
+        .setFooter("Thank you");
+      message.reply(b);
+    }
+    function send() {
+      let usr = message.mentions.users.first();
+      if (!usr) return message.reply("please mention user");
+      let text = message.content.split(" ").slice(2);
+      if (!text) return message.reply("Cant do that");
+      usr.send(text.join(" "));
+    }
   }
-}
+};
+
+
