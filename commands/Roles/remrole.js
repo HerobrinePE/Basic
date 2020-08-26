9e903dda8d3c772be4991b9732f5dbb877a253b0
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-08-26 11:03:26 -0400

    Update remrole.js

--- commands/Roles/remrole.js
+++ commands/Roles/remrole.js
@@ -25,27 +25,7 @@ module.exports = {
         .setColor("RANDOM")
         message.channel.send(bed)
         user.send(`${user} you have just lost the ${role.name}`)
-        function check() {
-          if (message.author.id == guid.owner.id) return everyone();
-          function everyone() {
-            let role = message.mentions.roles.first() || args[1]
-            let my = message.guild.roles.find("name", role)
-            if(!my) return message.reply("role not found")
-            message.guild.members.forEach(member => {
-              member.removeRoles(role);
-            });
-            const bed = new RichEmbed()
-            .setTitle("Server Updated")
-            .setDescription(`${guid.owner} has taken away ${role} from all members`)
-            .setColor("RANDOM")
-            message.channel.send(bed)
-          }
-          const bed = new RichEmbed()
-          .setTitle("Owner Only")
-          .setDescription(`only the server owner ${guid.owner} has access to role everyone`)
-          message.channel.send(bed);
-        }
-      }
+        
     } catch (e) {
       const errorbed = new RichEmbed().setDescription(e);
       message.channel.send(errorbed);
