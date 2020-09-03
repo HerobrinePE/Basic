9a2e2be6e1b7eff25066b10924d61438e32fedf2
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-09-03 18:03:09 -0400

    Create Tracker.js

---
+++ commands/Covid/Tracker.js
@@ -0,0 +1,22 @@
+const {RichEmbed} = require("discord.js")
+const track = require("covid19-stats")
+module.exports = {
+  name: "covid",
+  category:"Covid",
+  run: async(client, message, args) => {
+    let msg = message.content.split(" ").slice(1)
+    let m = msg.join(" ")
+    console.log(m.toUpperCase())
+    let covid = await track.getCountry(m.toUpperCase())
+    if (!covid) return message.reply("country not found")
+    console.log(covid)
+    const bed = new RichEmbed()
+    .setTitle("Covid Tracker")
+    .addField("Country", covid.country)
+    .addField("Total Cases", covid.totalCases)
+    .addField("New Cases", covid.newCases)
+    .addField("Total Deaths", covid.totalDeaths)
+    .addField("New Deaths", covid.newDeaths)
+    message.channel.send(bed)
+  }
+};
