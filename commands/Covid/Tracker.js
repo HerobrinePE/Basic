75d87a97bf4833006991bdc142e42f0d7dc078c5
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-09-04 19:50:00 -0400

    Update Tracker.js

--- commands/Covid/Tracker.js
+++ commands/Covid/Tracker.js
@@ -11,11 +11,40 @@ module.exports = {
     let msg = message.content.split(" ").slice(1);
     let m = msg.join(" ");
     let usr = message.author;
-    let r = args[0]
+    let r = args[0];
     if (r.toLowerCase() == "world") {
       api.all().then(val => {
+        console.log(val);
         const bed = new RichEmbed()
           .setTitle("☣Covid WorldWide☣")
+          .addField("API Updates per usage", val.updated)
+          .addField("Earths Population", val.population)
+          .setColor("#FF0000")
+          .addField("Cases Today", val.todayCases)
+          .addField("Total Cases", val.cases)
+          .addField("Deaths", val.deaths)
+          .addField("Deaths Today", val.todayDeaths)
+          .addField("Recoveries", val.recovered)
+          .addField("Recoveries today", val.todayRecovered)
+          .addField("Test Done", val.tests)
+          .addField("Active", val.active)
+          .addField("Critical Condition", val.critical)
+          .addField(
+            "Coding NodeJS",
+            "package used on this command [covidapi](https://www.npmjs.com/package/covidapi)"
+          );
+        message.channel.send(bed);
+      });
+    } else {
+      let countries = api.countries({ country: m.toLowerCase() }).then(val => {
+        let embed = new RichEmbed()
+          .setThumbnail(val.countryInfo.flag)
+          .setTitle(
+            "☣Covid " +
+              `[${val.country}] [${val.countryInfo.iso2}] [${val.countryInfo.iso3}] ` +
+              "☣"
+          )
+          .addField("Population", val.population)
           .addField("Updated", val.updated)
           .addField("Cases Today", val.todayCases)
           .addField("Total Cases", val.cases)
@@ -24,32 +53,14 @@ module.exports = {
           .addField("Recoveries", val.recovered)
           .addField("Recoveries today", val.todayRecovered)
           .addField("Active", val.active)
+        .addField("Critical", val.critical)
           .addField(
             "Coding NodeJS",
             "package used on this command [covidapi](https://www.npmjs.com/package/covidapi)"
           );
-        message.channel.send(bed);
+        message.channel.send(embed);
+        console.log(val);
       });
     }
-
-   let countries =  api.countries({ country: m.toLowerCase() }).then(val => {
-      let embed = new RichEmbed()
-        .setThumbnail(val.countryInfo.flag)
-        .setTitle("☣Covid "+`[${val.country}] [${val.countryInfo.iso2}] [${val.countryInfo.iso3}] `+"☣")
-        .addField("Updated", val.updated)
-        .addField("Cases Today", val.todayCases)
-        .addField("Total Cases", val.cases)
-        .addField("Deaths", val.deaths)
-        .addField("Deaths Today", val.todayDeaths)
-        .addField("Recoveries", val.recovered)
-        .addField("Recoveries today", val.todayRecovered)
-        .addField("Active", val.active)
-        .addField(
-          "Coding NodeJS",
-          "package used on this command [covidapi](https://www.npmjs.com/package/covidapi)"
-        );
-      message.channel.send(embed);
-      console.log(val);
-    });
   }
 };
