ede265e2564ada191b618901fa689930b5f95010
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-09-25 00:42:14 -0400

    Create colorGeneration.js

---
+++ commands/Fun/colorGeneration.js
@@ -0,0 +1,49 @@
+const { RichEmbed, MessageAttachment } = require("discord.js");
+const generate = require("jimp");
+const fs = require("fs");
+module.exports = {
+  name: "discolor",
+  category: "Fun",
+  description: "Inverts users image",
+  run: async (client, message, args) => {
+    let usr = message.mentions.users.first();
+    let aut = message.author;
+    if (usr) {
+      generate.read(usr.avatarURL).then(image => {
+        image
+          .quality(60)
+          .resize(250, 250)
+          .normalize()
+        .color([
+  { apply: 'hue', params: [Math.floor(Math.random() * -100)] },
+  { apply: 'lighten', params: [Math.floor(Math.random()*50)] },
+  { apply: 'xor', params: [Math.floor(Math.random()*16777215).toString(16)] }
+])
+          .write("test.png");
+        message.channel
+          .send(``, { files: ["test.png"] })
+          .then(fs.unlink("/test.png"));
+      });
+    } else {
+      await generate
+        .read(aut.avatarURL)
+        .then(image => {
+          image
+            .quality(100)
+            .resize(250, 250)
+      .color([
+  { apply: 'hue', params: [Math.floor(Math.random() * -100)] },
+  { apply: 'lighten', params: [Math.floor(Math.random()*50)] },
+  { apply: 'xor', params: [Math.floor(Math.random()*16777215).toString(16)] }
+])
+            .write("test.png");
+          message.channel
+            .send(``, { files: ["test.png"] })
+            .then(fs.unlink("/test.png"));
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+};
